ea4d9a275b548281bd4b9226437086845bc8427a
brawlingNavada <JCLacivita@gmail.com>
2017-02-03 14:58:45 -0700

    working with wx data

--- app/components/weather/weather-controller.js
+++ app/components/weather/weather-controller.js
@@ -5,19 +5,53 @@
 	
 	weatherService.getWeather(function(weather){
 		//console.log(weather);
+		//console.log("type of weather: :",typeof weather)
 		//What can you do with this weather object?
 
 		new Vue({
 			el: '#weather',
-			data:{
-				weather: weather
+			data:
+			{
+				//weather: weather,
+				weather: 'wx data',
+				where: 'WhereData',
+				tempK: 'KelvenData',
+				tempC: 'CelsusData',
+				tempF: 'FahData',
+			},
+			
+			mounted: function(){this.getTemp()},
+
+			methods:
+			{
+				getName: function()
+				{
+					//console.log("in weather methods")
+					//console.log(typeof this.weather)
+					weatherObject = JSON.parse(weather)
+					//console.log(typeof weatherObject)
+					//console.log(weatherObject['name'])
+					this.where = weatherObject['name']
+					console.log(this.where)
+				},
+
+				getTemp: function()
+				{
+				console.log("in getTemp")
+				weatherObject = JSON.parse(weather)
+				this.tempK = weatherObject['main']['temp']	
+				this.tempK = this.tempK.toFixed(0)
+				this.tempF = (9/5)*(this.tempK-273)+32
+				this.tempF = this.tempF.toFixed(0)	
+				this.tempC = this.tempK-273
+				this.tempC = this.tempC.toFixed(0)
+				}
 			}
 		})
 
 	})
 	
-	
-	
+
 	
 	
 }())
--- app/components/weather/weather-service.js
+++ app/components/weather/weather-service.js
@@ -3,7 +3,7 @@ function WeatherService() {
 	var url2 = 'http://api.openweathermap.org/data/2.5/weather?q=boise&&APPID=bd82255fd0a21fa1238699b9eda2ee35'
 	var apiUrl = url + encodeURIComponent(url2);
 
-
+	console.log(apiUrl)
 	this.getWeather = function (callWhenDone) {
 		var w = localStorage.getItem('weather');
 		if (w) {
