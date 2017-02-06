cb833ad8373c048a0be3fc12daa95a88f1d657a4
brawlingNavada <JCLacivita@gmail.com>
2017-02-06 09:45:56 -0700

    fixed weather data

--- app/components/weather/weather-service.js
+++ app/components/weather/weather-service.js
@@ -6,12 +6,6 @@ function WeatherService() {
 
 	//console.log(apiUrl)
 	this.getWeather = function (callWhenDone) {
-		var w = localStorage.getItem('weather');
-		if (w) {
-			w = JSON.parse(w);
-			//console.log('Weather Data:', w)
-			return callWhenDone(w)
-		}
 
 		$.get(apiUrl, function (res) {
 			localStorage.setItem('weather', JSON.stringify(res))
